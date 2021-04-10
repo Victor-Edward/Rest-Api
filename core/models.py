52a24a497919d8fc0b202af8b5edf49d7702484b
Victor Edward <victoredward30@gmail.com>
2021-04-10 20:45:51 -0300

    first commit

---
+++ core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    endereço = models.CharField(max_length=80)
+    idade= models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
